e1353d270d6e80746a6bbd64829deb1665bc652d
nicolas manoharan <nicolas.manoharan@gmail.com>
2021-09-27 15:56:40 +0200

    activation de l'intégration continu

--- dev_history.R
+++ dev_history.R
@@ -2,5 +2,4 @@ usethis::use_build_ignore("dev_history.R")
 
 usethis::use_gpl_license()
 
-# Créer une vignette pour documenter l'utilisation du package
-usethis::use_vignette("dire-bonjour")
+usethis::use_git()
